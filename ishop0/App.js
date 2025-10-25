2e349eb14d394dcd5e50f90396ff6b636f7e510e
OlMr3 <olenka.mryazeva@mail.ru>
2025-10-25 15:06:21 +0300

    Delete ishop0 directory

--- ishop0/App.js
+++
@@ -1,9 +0,0 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Shop  from './components/Warning';
-
-ReactDOM.render(
-   <Shop name="Название интернет-магазина" address="Адрес интернет-магазина" />,
-  document.getElementById('container') 
-);
