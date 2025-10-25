ec8db65f1bdacd4094f270a8575dd12cd95fc9f6
OlMr3 <olenka.mryazeva@mail.ru>
2025-10-25 15:24:58 +0300

    new shop0

---
+++ Shop0/components/Shop.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+
+import './Shop.css';
+
+class Shop extends React.Component {
+
+  render() {
+    return (
+      <div className='Shop'>
+        <h1 className='Shop_Name'>
+          {this.props.name}
+        </h1>
+        <span className='Shop_Address'>
+          {this.props.address}
+        </span>
+      </div>
+    );
+  }
+
+}
+
+export default Shop;
