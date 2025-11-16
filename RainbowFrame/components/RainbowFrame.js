83e132d1cde866e0b8270d67574f97f68e2e1c41
OlMr3 <olenka.mryazeva@mail.ru>
2025-11-16 13:18:09 +0300

    dz RainbowFrame

---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+  static propTypes= {
+    colors: PropTypes.array,
+  };
+  buildFrames(colors, children) {
+    return colors.reduceRight((acc, color) => (
+      <div 
+      key = {color}
+      style={{
+          border: `10px solid ${color}`
+        }}
+        className='RainbowFrame'
+      >
+        {acc}
+      </div>
+
+    ), children);
+  }
+render(){
+  const {colors, children} = this.props;
+  return this.buildFrames(colors, children);
+}
+
+}
+
+export default RainbowFrame;
