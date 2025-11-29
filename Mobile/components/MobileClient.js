72ae62b57bfd83f67ce234b8b0f7cd25043b00ee
OlMr3 <olenka.mryazeva@mail.ru>
2025-11-29 21:00:20 +0300

    mobile

---
+++ Mobile/components/MobileClient.js
@@ -0,0 +1,113 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import { mobileEvent } from './events';
+import './MobileClient.css';
+
+class MobileClient extends React.PureComponent {
+  static propTypes = {
+    client: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      fam: PropTypes.string.isRequired,
+      im: PropTypes.string.isRequired,
+      otch: PropTypes.string.isRequired,
+      balance: PropTypes.number.isRequired,
+      isNew: PropTypes.bool,
+      onEdit: PropTypes.func 
+    }).isRequired,
+    isNew: PropTypes.bool
+  };
+  constructor(props) {
+    super(props);
+    this.state = { isEditing: props.isNew || false};
+    this.famRef = React.createRef();
+    this.imRef = React.createRef();
+    this.otchRef = React.createRef();
+    this.balanceRef = React.createRef();
+
+  }
+componentDidMount() {
+    if (this.props.isNew) {
+      this.setState({ isEditing: true });
+    }
+  }
+componentDidUpdate(prevProps) {
+  if (prevProps.isNew && !this.props.isNew) {
+    this.setState({ isEditing: false });
+  }
+}
+
+ handleEdit = () => {
+    this.setState({ isEditing: true });
+    if (this.props.onEdit) {
+      this.props.onEdit(); 
+    }
+  };
+
+  handleCancel = () => {
+    this.setState({ isEditing: false });
+    mobileEvent.emit('cancelEdit', this.props.client.id);
+  };
+
+  handleSave = () => {
+    const newFam = this.famRef.current.value;
+    const newIm = this.imRef.current.value;
+    const newOtch = this.otchRef.current.value;
+    const newBalance = parseFloat(this.balanceRef.current.value);
+    mobileEvent.emit('editClientField', { clientId: this.props.client.id, field: 'fam', value: newFam });
+    mobileEvent.emit('editClientField', { clientId: this.props.client.id, field: 'im', value: newIm });
+    mobileEvent.emit('editClientField', { clientId: this.props.client.id, field: 'otch', value: newOtch });
+    mobileEvent.emit('editClientField', { clientId: this.props.client.id, field: 'balance', value: newBalance });
+    this.setState({isEditing: false});
+  };
+
+  handleDelete = () => {
+    mobileEvent.emit('deleteClient', this.props.client.id);
+  };
+
+  render() {
+
+    console.log(`MobileClient id=${this.props.client.id} render`);
+    const c = this.props.client;
+    const editing = this.state.isEditing;
+    const status = c.balance >= 0 ? 'active' : 'blocked';
+
+    return (
+      <div className='MobileClient'>
+        {editing ? (
+          <React.Fragment>
+            <input defaultValue={c.fam} ref={this.famRef} />{' '}
+            <input defaultValue={c.im} ref={this.imRef} />{' '}
+            <input defaultValue={c.otch} ref={this.otchRef} />{' '}
+            <input type='number' defaultValue={c.balance} ref={this.balanceRef} />
+            <button onClick={this.handleSave}>Сохранить</button>
+            <button onClick={this.handleCancel}>Отмена</button>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+
+            <div className='MobileClientFIO'>
+              <div>{c.fam}</div>
+              <div>{c.im}</div>
+              <div>{c.otch}</div>
+
+
+            </div>
+            <div className='MobileClientBalance'>{c.balance} </div>
+            <div className='MobileClientBalance'>  {status === 'blocked' ? (
+              <span className='status-blocked'>({status})</span>
+            ) : (
+              <span>({status})</span>
+            )}</div>
+
+            <button className='Button' onClick={this.handleEdit}>Редактировать</button>
+            <button onClick={this.handleDelete}>Удалить</button>
+          </React.Fragment>
+        )}
+      </div>
+    );
+
+  }
+
+}
+
+export default MobileClient;
---
+++ Mobile/components/MobileCompany.js
@@ -0,0 +1,150 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import {mobileEvent} from './events';
+import MobileClient from './MobileClient';
+
+import './MobileCompany.css';
+
+class MobileCompany extends React.PureComponent {
+
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    clients:PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        fam: PropTypes.string.isRequired,
+        im: PropTypes.string.isRequired,
+        otch: PropTypes.string.isRequired,
+        balance: PropTypes.number.isRequired,
+        
+      })
+    ),
+  };
+
+  state = {
+    name: this.props.name,
+    clients: [...this.props.clients],
+    filter: 'all',
+    editingClientId: null,
+  };
+
+  setName1 = () => {
+    this.setState({name:'МТС'});
+  };
+
+  setName2 = () => {
+    this.setState({name:'A1'});
+  };
+   componentDidMount() {
+    mobileEvent.on('deleteClient', this.handleDelete);
+    mobileEvent.on('editClientField', this.handleEditField);
+    mobileEvent.on('saveClient', this.handleSave);
+    mobileEvent.on('cancelEdit', this.handleCancel);
+    mobileEvent.on('addClient', this.handleAddClient);
+  }
+
+  componentWillUnmount() {
+    mobileEvent.off('deleteClient', this.handleDelete);
+    mobileEvent.off('editClientField', this.handleEditField);
+    mobileEvent.off('saveClient', this.handleSave);
+    mobileEvent.off('cancelEdit', this.handleCancel);
+    mobileEvent.off('addClient', this.handleAddClient);
+  }
+
+  handleDelete = (clientId) => {
+    this.setState((prevState) => {
+      const newClients = prevState.clients.filter(c => c.id !== clientId);
+      return { clients: newClients };
+    });
+  };
+
+  handleEditField = ({ clientId, field, value }) => {
+    this.setState(prev => {
+      const newClients = prev.clients.map(c => {
+        if (c.id === clientId) {
+          return { ...c, [field]: value, isNew: false };
+        }
+        return c;
+      });
+      return { clients: newClients };
+    });
+  };
+
+ 
+  handleAddClient = () => {
+  
+    const maxId = this.state.clients.reduce((max, c) => Math.max(max, c.id), 0);
+    const newClient = {
+      id: maxId + 1,
+      fam: '',
+      im: '',
+      otch: '',
+      balance: 0,
+      isNew: true,
+    };
+    this.setState(prev => ({
+      clients: [...prev.clients, newClient],
+      editingClientId: newClient.id,
+    }));
+  };
+
+  setFilter = (filter) => {
+    this.setState({ filter, editingClientId: null });
+  };
+   handleEditClient = (clientId) => {
+    this.setState({ editingClientId: clientId });
+  };
+  
+  render() {
+
+    console.log("MobileCompany render");
+    const { clients, filter, editingClientId } = this.state;
+let filteredClients = clients;
+    if (filter === 'active') {
+      filteredClients = clients.filter(c => c.balance > 0);
+    } else if (filter === 'blocked') {
+      filteredClients = clients.filter(c => c.balance <= 0);
+    }
+
+    const clientsCode = filteredClients.map(client => (
+      <MobileClient 
+      key={client.id} 
+      client={client} 
+      isNew={client.isNew} 
+      isEditing={client.id === this.state.editingClientId }  
+      onEdit={() => this.handleEditClient(client.id)}/>
+    ));
+
+    return (
+      <div className='MobileCompany'>
+        <input type="button" value="=МТС" onClick={this.setName1} />
+        <input type="button" value="=A1" onClick={this.setName2} />
+        <div className='MobileCompanyName'>Компания &laquo;{this.state.name}&raquo;</div>
+        <div>
+          <button onClick={() => this.setFilter('all')}>Все</button>
+          <button onClick={() => this.setFilter('active')}>Активные</button>
+          <button onClick={() => this.setFilter('blocked')}>Заблокированные</button>
+        </div>
+        <div className='Table'>
+          <div>Фамилия</div>
+          <div>Имя</div>
+          <div>Отчество</div>
+          <div>Баланс</div>
+          <div>Статус</div>
+          <div>Редактировать</div>
+          <div>Удалить</div>
+
+        </div>
+        <div className='MobileCompanyClients'>
+          {clientsCode}
+        </div>
+        <button onClick={() => mobileEvent.emit('addClient')}>Добавить клиента</button>
+      </div>
+    )
+    ;
+
+  }
+
+}
+
+export default MobileCompany;
